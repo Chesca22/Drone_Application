601b6417f7b82c4377dc7363992ae64a554ded0c
francisca <francisca.ikpehare@decagon.dev>
2022-11-21 21:41:08 +0100

    created controller class, exception controller and installed openAPI dependency

---
+++ src/main/java/com/francisca/droneService/controller/DroneController.java
@@ -0,0 +1,94 @@
+package com.francisca.droneService.controller;
+
+import com.francisca.droneService.dto.DroneDeliveryDto;
+import com.francisca.droneService.dto.DroneDto;
+import com.francisca.droneService.dto.MedicationDto;
+import com.francisca.droneService.service.DroneService;
+import com.francisca.droneService.service.MedicationService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@Getter
+@Slf4j
+public class DroneController {
+    private final DroneService droneService;
+    private final MedicationService medicationService;
+    @Operation(summary = "Registering a drone")
+    @PostMapping("/register-drone")
+    public ResponseEntity<?> registerDrone(@RequestBody DroneDto dto){
+        log.info("success");
+     return new ResponseEntity<>(droneService.registerDrone(dto), HttpStatus.CREATED);
+    }
+
+    @Operation(summary = "Get the list of all Drones")
+    @GetMapping("/all-drones")
+    public ResponseEntity<?> getAllDrones(){
+        return new ResponseEntity<>(droneService.getAllDrones(), OK);
+    }
+
+    @Operation(summary = "Save a medication")
+    @PostMapping("/save-medicine")
+    public ResponseEntity<?> saveMedicineDetails(@RequestBody MedicationDto medicationDto) {
+        log.info("success");
+        return new ResponseEntity<>(medicationService.medicationDetails(medicationDto), HttpStatus.CREATED);
+    }
+
+    @Operation(summary = "view the list of all medications")
+    @GetMapping("/all-medication")
+    public ResponseEntity<?> getAllMedication(){
+      return new ResponseEntity<>(medicationService.listOfMedications(), OK);
+    }
+
+    @Operation(summary = "Getting the list of all Idle drones")
+    @GetMapping("/available-drone")
+    public ResponseEntity<?> AvailableDrone(){
+        log.info("available drones retrieve successfully");
+        return new ResponseEntity<>(droneService.getAvailableDrones(), OK);
+    }
+
+    @Operation(summary = "Getting a single available drone")
+    @GetMapping("/get_drone/{drone_no}")
+    public ResponseEntity<?> findAnIdleDrone(@PathVariable(value="drone_no") String serialNumber){
+        log.info("drone found successfully");
+        return new ResponseEntity<>(droneService.getADrone(serialNumber), OK);
+    }
+
+    @Operation(summary = "Loading a drone with a medication")
+    @GetMapping("/load-drone/{drone_no}/{med-code}")
+    public ResponseEntity<?> loadingDrone(@PathVariable(value="drone_no") String serialNumber, @PathVariable(value="med-code")String code){
+        log.info("drone loaded successfully");
+        return new ResponseEntity<>(droneService.loadDrone(serialNumber, code), HttpStatus.OK);
+    }
+
+    @Operation(summary = "Delivering a drone with a medication")
+    @PostMapping("/droneDelivery")
+    public ResponseEntity<?> DeliverMedication( @RequestBody DroneDeliveryDto dto){
+        log.info("delivered");
+        return new ResponseEntity<>(droneService.deliverMedication(dto), HttpStatus.OK);
+    }
+    @Operation(summary = "Getting the list of loaded medications on a drone")
+    @GetMapping("/loaded-medication/{drone_no}")
+    public ResponseEntity<?> getLoadedMedication(@PathVariable(value="drone_no") String serialNo){
+        log.info("loaded medication retrieved successfully");
+        return new ResponseEntity<>(droneService.getLoadedMedication(serialNo), OK);
+    }
+
+    @Operation(summary = "Viewing the battery level of a drone")
+    @GetMapping("/battery-level/{drone_no}")
+    public ResponseEntity<?> batteryLevelOfADrone(@PathVariable(value="drone_no") String serialNo){
+        log.info("successfully");
+        return new ResponseEntity<>(droneService.checkBatteryLevel(serialNo), OK);
+    }
+
+
+}
---
+++ src/main/java/com/francisca/droneService/controller/ExceptionController.java
@@ -0,0 +1,44 @@
+package com.francisca.droneService.controller;
+
+import com.francisca.droneService.exception.*;
+import com.francisca.droneService.response.ExceptionResponse;
+import com.francisca.droneService.service.ResponseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+@RequiredArgsConstructor
+public class ExceptionController {
+
+    private final ResponseService<ExceptionResponse> responseService;
+
+    @ExceptionHandler(DroneAlreadyRegisteredException.class)
+    public ResponseEntity<ExceptionResponse> resourceNotFoundHandler(DroneAlreadyRegisteredException exception){
+        return responseService.response(new ExceptionResponse(exception.getMessage(), LocalDateTime.now()), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(BatteryAndDroneStateException.class)
+    public ResponseEntity<ExceptionResponse> resourceNotFoundHandler(BatteryAndDroneStateException exception){
+        return responseService.response(new ExceptionResponse(exception.getMessage(), LocalDateTime.now()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MedicineDetailsAlreadyExistException.class)
+    public ResponseEntity<ExceptionResponse> resourceNotFoundHandler(MedicineDetailsAlreadyExistException exception){
+        return responseService.response(new ExceptionResponse(exception.getMessage(), LocalDateTime.now()), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(NoAvailableDroneFoundException.class)
+    public ResponseEntity<ExceptionResponse> resourceNotFoundHandler(NoAvailableDroneFoundException exception){
+        return responseService.response(new ExceptionResponse(exception.getMessage(), LocalDateTime.now()), HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(DroneLimitException.class)
+    public ResponseEntity<ExceptionResponse> droneLimitHandler(DroneLimitException exception){
+        return responseService.response(new ExceptionResponse(exception.getMessage(), LocalDateTime.now()), HttpStatus.NOT_FOUND);
+    }
+
+}
